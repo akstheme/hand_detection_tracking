902fe4054fcade33a419a351b642af330b870fbe
Dr. Kuldeep S. Yadav <123442534+akstheme@users.noreply.github.com>
2024-05-29 01:08:23 +0530

    Create main.m

---
+++ main.m
@@ -0,0 +1,75 @@
+clc;
+close all;
+clear all;
+
+
+% Read a video frame and detect the hand.
+videoFileReader = vision.VideoFileReader('gesture.mp4');
+videoFrame = step(videoFileReader);
+[Box,Img]=Hand_seg(videoFrame);
+bbox  = round(Box.Position);
+
+% Draw the returned bounding box around the detected hand.
+videoFrame = insertShape(videoFrame, 'Rectangle', bbox);
+figure; imshow(videoFrame); title('Detected face');
+
+% Convert the first box into a list of 4 points
+% This is needed to be able to visualize the rotation of the object.
+bboxPoints = bbox2points(bbox(1, :));
+% points = detectMinEigenFeatures(rgb2gray(videoFrame), 'ROI', bbox(1,:));
+points = detectHarrisFeatures(rgb2gray(videoFrame), 'ROI', bbox(1,:));
+% Display the detected points.
+figure, imshow(videoFrame), hold on, title('Detected features');
+plot(points);
+pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
+
+% Initialize the tracker with the initial point locations and the initial
+% video frame.
+points = points.Location;
+initialize(pointTracker, points, videoFrame);
+videoPlayer  = vision.VideoPlayer('Position',...
+    [100 100 [size(videoFrame, 2), size(videoFrame, 1)]+30]);
+
+oldPoints = points;
+
+while ~isDone(videoFileReader)
+    % get the next frame
+    videoFrame = step(videoFileReader);
+
+    % Track the points. Note that some points may be lost.
+    [points, isFound] = step(pointTracker, videoFrame);
+    visiblePoints = points(isFound, :);
+    oldInliers = oldPoints(isFound, :);
+    
+    if size(visiblePoints, 1) >= 2 % need at least 2 points
+        
+        % Estimate the geometric transformation between the old points
+        % and the new points and eliminate outliers
+        [xform, oldInliers, visiblePoints] = estimateGeometricTransform(...
+            oldInliers, visiblePoints, 'similarity', 'MaxDistance', 4);
+        
+        % Apply the transformation to the bounding box points
+        bboxPoints = transformPointsForward(xform, bboxPoints);
+                
+        % Insert a bounding box around the object being tracked
+        bboxPolygon = reshape(bboxPoints', 1, []);
+        videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, ...
+            'LineWidth', 2);
+                
+        % Display tracked points
+        videoFrame = insertMarker(videoFrame, visiblePoints, '+', ...
+            'Color', 'white');       
+        
+        % Reset the points
+        oldPoints = visiblePoints;
+        setPoints(pointTracker, oldPoints);        
+    end
+    
+    % Display the annotated video frame using the video player object
+    step(videoPlayer, videoFrame);
+end
+
+% Clean up
+release(videoFileReader);
+release(videoPlayer);
+release(pointTracker);
